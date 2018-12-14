8c38d16cb61b3339a9b6a1d100113c9f65737e5a
Ubuntu <ubuntu@ip-172-31-26-247.eu-west-3.compute.internal>
2018-12-14 15:24:01 +0000

    debug, elimination of some file that weirdly lacks exactly one field

--- evaluate_results.py
+++ evaluate_results.py
@@ -7,6 +7,7 @@ import os
 
 QUESTION_GROUP_TYPES = ['read_group','edit_group','uninterested_group']
 N_PAIRS = 6
+GLOBAL_MISSING_FIELD_COUNT = 0
 
 def order_q_based_first(name_and_value):
     pair_name = name_and_value[0]
@@ -20,6 +21,7 @@ def order_q_based_first(name_and_value):
         return [new_name, new_value]
 
 def convert_json_to_results(json_file):
+    global GLOBAL_MISSING_FIELD_COUNT
     print(json_file)
     recorded_data = json.load(open(json_file, mode='r', encoding='utf8'))
 
@@ -40,15 +42,24 @@ def convert_json_to_results(json_file):
         subfields = {int(x.strip(question_type_name)): subfields[x] for x in subfields}
         # Now create a list of lists with the name of the recom type pair to the value,
         # inverting the list (and negating the value) if q_based hasn't come first.
-        subfields = [order_q_based_first([comparison_pair_names[x], subfields[x]]) for x in range(N_PAIRS)]
+        GLOBAL_MISSING_FIELD_COUNT += N_PAIRS - len(subfields)
+        if len(subfields) < N_PAIRS:
+            return None
+        subfields = [order_q_based_first([comparison_pair_names[x], subfields[x]]) for x in range(N_PAIRS) if x in subfields]
         results_dict[question_type_name] = subfields
 
     return results_dict
 
 def get_all_results(list_of_files):
     result_list = []
+    count = 0
     for filename in list_of_files:
-        result_list.append(convert_json_to_results(filename))
+        current_result = convert_json_to_results(filename)
+        if current_result is not None:
+            result_list.append(current_result)
+        count += 1
+        if count % 10 == 0 and count != 0:
+            print(GLOBAL_MISSING_FIELD_COUNT)
     return result_list
 
 def data_to_dataframe(results_list):
@@ -60,7 +71,7 @@ def data_to_dataframe(results_list):
             for question_type in QUESTION_GROUP_TYPES:
                 current_list.append(current_result[question_type][i][1])
             list_of_lists.append(current_list)
-    column_names = ['name']
+    column_names = ['comparison_name']
     column_names.extend(QUESTION_GROUP_TYPES)
     return pd.DataFrame(list_of_lists, columns=column_names)
 
@@ -73,18 +84,27 @@ def sign(x):
         return 0
 
 def get_descriptive_stats(df):
+    print(df.describe())
     for question_type in QUESTION_GROUP_TYPES:
         df[question_type] = df[question_type].apply(sign)
     print('Number of "wins", "draws" and "losses" vs each baseline:')
-    print(df.groupby('name').apply(lambda x: dict(Counter(list(x)))))
+    df_agg = df.groupby('comparison_name').apply(lambda x: dict(Counter(list(x))))
+    for i in range(df_agg.shape[0]):
+        print(df_agg.iloc[i])
+        for question_type in QUESTION_GROUP_TYPES:
+            for current_value in range(-1,2):
+                print((df_agg.iloc[i, question_type])[current_value])
 
 def main():
+    global GLOBAL_MISSING_FIELD_COUNT
     input_dir = sys.argv[1]
     starting_day = 12
     filenames_list = os.listdir(input_dir)
     filenames_list = [x for x in filenames_list if int(x.split('_')[2]) >= starting_day]
     filenames_list = [os.path.join(input_dir, x) for x in filenames_list]
     get_descriptive_stats(data_to_dataframe(get_all_results(filenames_list)))
+    print('Number of JSON files with missing values:')
+    print(GLOBAL_MISSING_FIELD_COUNT)
 
 if __name__ == '__main__':
     main()
