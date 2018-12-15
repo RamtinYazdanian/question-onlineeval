376820b7756d76df23e5e592a65712e3cb541ff6
RamtinYazdanian <ramtin.yazdanian@gmail.com>
2018-12-15 09:31:22 +0100

    debug

--- anonymise_results.py
+++ anonymise_results.py
@@ -4,6 +4,7 @@ import sys
 
 def remove_username_field(d):
     if d['name_field'] != 'THIS_USER_WAS_NOT_SUPPOSED_TO_BE_IN_THE_STUDY_CAUSE_NO_TOKEN_YEAH_SUCKS':
+        print(d['name_field'])
         d['name_field'] = 'REDACTED'
 
 def main():
@@ -11,6 +12,7 @@ def main():
     all_filenames = os.listdir(input_dir)
     all_jsons = dict()
     for x in all_filenames:
+        print(x)
         with open(os.path.join(input_dir, x), 'r') as f:
             all_jsons[x] = json.load(f)
     with_username_removed = {x: remove_username_field(all_jsons[x]) for x in all_jsons}
