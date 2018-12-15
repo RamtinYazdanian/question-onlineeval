0099dcb842148b73a0bff91c6a2ea93dee73673c
RamtinYazdanian <ramtin.yazdanian@gmail.com>
2018-12-15 09:26:38 +0100

    anonymising the results in order to take a backup

---
+++ anonymise_results.py
@@ -0,0 +1,19 @@
+import json
+import os
+import sys
+
+def remove_username_field(d):
+    if d['name_field'] != 'THIS_USER_WAS_NOT_SUPPOSED_TO_BE_IN_THE_STUDY_CAUSE_NO_TOKEN_YEAH_SUCKS':
+        d['name_field'] = 'REDACTED'
+
+def main():
+    input_dir = sys.argv[1]
+    all_filenames = os.listdir(input_dir)
+    all_jsons = {x: json.load(open(os.path.join(input_dir, x), 'r')) for x in all_filenames}
+    with_username_removed = {x: remove_username_field(all_jsons[x]) for x in all_jsons}
+    for x in with_username_removed:
+        with open(os.path.join(input_dir, x), 'w') as f:
+            json.dump(with_username_removed[x], f)
+
+if __name__ == '__main__':
+    main()
