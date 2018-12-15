2c1ceedb5b8dfcea10296f05c33995aad82333d4
RamtinYazdanian <ramtin.yazdanian@gmail.com>
2018-12-15 09:29:41 +0100

    debug

--- anonymise_results.py
+++ anonymise_results.py
@@ -9,7 +9,10 @@ def remove_username_field(d):
 def main():
     input_dir = sys.argv[1]
     all_filenames = os.listdir(input_dir)
-    all_jsons = {x: json.load(open(os.path.join(input_dir, x), 'r')) for x in all_filenames}
+    all_jsons = dict()
+    for x in all_filenames:
+        with open(os.path.join(input_dir, x), 'r') as f:
+            all_jsons[x] = json.load(f)
     with_username_removed = {x: remove_username_field(all_jsons[x]) for x in all_jsons}
     for x in with_username_removed:
         with open(os.path.join(input_dir, x), 'w') as f:
