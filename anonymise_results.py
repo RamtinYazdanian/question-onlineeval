3c82afe6ae626ce1f0900c7c7c5ab6ff230a7e81
RamtinYazdanian <ramtin.yazdanian@gmail.com>
2018-12-18 12:41:11 +0100

    anonymising the filenames as well

--- anonymise_results.py
+++ anonymise_results.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 from common_utils import make_sure_path_exists
+import hashlib
 
 def remove_username_field(d):
     if d['name_field'] != 'THIS_USER_WAS_NOT_SUPPOSED_TO_BE_IN_THE_STUDY_CAUSE_NO_TOKEN_YEAH_SUCKS':
@@ -12,7 +13,9 @@ def remove_username_field(d):
 def main():
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
+    starting_day = 12
     all_filenames = os.listdir(input_dir)
+    all_filenames = [x for x in all_filenames if int(x.split('_')[2]) >= starting_day]
     all_jsons = dict()
     for x in all_filenames:
         print(x)
@@ -21,7 +24,7 @@ def main():
     with_username_removed = {x: remove_username_field(all_jsons[x]) for x in all_jsons}
     make_sure_path_exists(output_dir)
     for x in with_username_removed:
-        with open(os.path.join(output_dir, 'anon_'+x), 'w') as f:
+        with open(os.path.join(output_dir, 'anon_'+hashlib.md5(x.encode('utf8')).hexdigest()), 'w') as f:
             json.dump(with_username_removed[x], f)
 
 if __name__ == '__main__':
