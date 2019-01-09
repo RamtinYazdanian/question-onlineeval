4716ea1c2a41740ce81fa6ee517711c853e49f15
RamtinYazdanian <ramtin.yazdanian@gmail.com>
2019-01-09 10:14:43 +0100

    counting the contributions of the user

--- anonymise_results.py
+++ anonymise_results.py
@@ -11,8 +11,15 @@ def remove_username_field(d):
     return d
 
 def main():
+    usage_str = 'Input dir (the JSON files), The JSON file containing each user\'s contributions, Output dir'
+    if len(sys.argv) != 4:
+        print(usage_str)
+        return
     input_dir = sys.argv[1]
-    output_dir = sys.argv[2]
+    contribs_filename = sys.argv[2]
+    output_dir = sys.argv[3]
+
+    contribs_dict = json.load(open(contribs_filename, 'r'))
     starting_day = 12
     starting_month = 12
     all_filenames = os.listdir(input_dir)
@@ -22,7 +29,13 @@ def main():
     for x in all_filenames:
         print(x)
         with open(os.path.join(input_dir, x), 'r') as f:
-            all_jsons[x] = json.load(f)
+            current_json = json.load(f)
+            current_username = current_json['name_field']
+            if current_username in contribs_dict:
+                current_json['contrib_count'] = len(contribs_dict[current_username])
+            else:
+                current_json['contrib_count'] = 0
+            all_jsons[x] = current_json
     with_username_removed = {x: remove_username_field(all_jsons[x]) for x in all_jsons}
     make_sure_path_exists(output_dir)
     for x in with_username_removed:
